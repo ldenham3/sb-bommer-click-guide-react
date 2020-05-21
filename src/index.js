edfa075c3d50d87049b1d83bd19ecc4e1c193e94
Leah Baker <leah@leahs-mbp.lan>
2020-05-20 18:26:48 -0700

    tried using reacts add react to project example but it didnt have jsx. committing without jsx before i try the instructions to add that

--- src/index.js
+++ src/index.js
@@ -8,3 +8,26 @@
 // import "github-fork-ribbon-css/gh-fork-ribbon.css";
 
 // ReactDOM.render(<App />, document.getElementById("root"));
+
+// 'use strict';
+
+// const e = React.createElement;
+
+// class LikeButton extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = { liked: false };
+//     }
+
+//     render() {
+//         if (this.state.liked) {
+//         return 'You liked this.';
+//         }
+
+//         return e(
+//             'button',
+//             { onClick: () => this.setState({ liked: true }) },
+//             'Like'
+//         );
+//     }
+// }
